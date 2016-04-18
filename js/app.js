7143a4318eddca454c3edb71ab8b5255dca36c7b
scottdiallo <diallot@me.com>
2016-04-18 08:44:10 -0700

    change drink constructor to order constructor

--- js/app.js
+++ js/app.js
@@ -1,19 +1,11 @@
-var drinkTypeHandler = function(){
-    // Will get values from the form
-    // to determine how the pirate makes the drink
-};
-var Drink = function(isStrong, isSalty, isBitter, isSweet, isFruity){
+
+var Order = function(isStrong, isSalty, isBitter, isSweet, isFruity){
     this.isStrong = isStrong;
     this.isSalty = isSalty;
     this.isBitter = isBitter;
     this.isSweet = isSweet;
     this.isFruity = isFruity;
-
 };
-
-// Note the new
-var Scott = new Person("Scott", 24, "San Fransico");
-
 
 $(document).ready(function(){
 
