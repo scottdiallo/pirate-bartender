a7118e068e48f78123bb143c6d3988ebbcddd88a
scottdiallo <diallot@me.com>
2016-04-18 08:25:23 -0700

    drink function add to js

--- js/app.js
+++ js/app.js
@@ -2,6 +2,18 @@ var drinkTypeHandler = function(){
     // Will get values from the form
     // to determine how the pirate makes the drink
 };
+var Drink = function(isStrong, isSalty, isBitter, isSweet, isFruity){
+    this.isStrong = isStrong;
+    this.isSalty = isSalty;
+    this.isBitter = isBitter;
+    this.isSweet = isSweet;
+    this.isFruity = isFruity;
+
+};
+
+// Note the new
+var Scott = new Person("Scott", 24, "San Fransico");
+
 
 $(document).ready(function(){
 
