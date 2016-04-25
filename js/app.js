36746b398288b9373db043324c37551b90fa59d4
Scott Diallo <diallot@me.com>
2016-04-24 18:49:03 -0700

    commented all js code for readability

--- js/app.js
+++ js/app.js
@@ -10,35 +10,36 @@ var Order = function(orderValues) {
 var Drink = function(pantry, drinkOrder) {
     var ingredientNumber,
         ingredientsArray = [];
-
+// formula to generate random drink based on user selection
     for (var userPreference in drinkOrder) {
         ingredientNumber = Math.floor(Math.random() * 2);
         if (drinkOrder[userPreference]) {
             ingredientsArray.push(pantry[userPreference][ingredientNumber]);
         }
     }
-    return ingredientsArray;
+    return ingredientsArray; // return back the drink made by random generator
 
 };
 
 var drinkNamer = function(concoction){
     // use if statements to piece together name conditionally
-    // based on the ingredts that comprise it
+    // based on the ingredients that comprise it
 };
 
 $(document).ready(function() {
+    // created an object with property that stored values in arrays.
     var concoction,
         pantry = {
-            strong: ["rum", "whiskey", "gin"],
+            strong: ["rum", "whiskey", "gin"], // if user say to any of these property it will pick a random based on the number given by computer to select the taste type
             salty: ["olive", "salt rim", "bacon"],
             bitter: ["bitters", "tonic", "lemon peel"],
             sweet: ["cola", "agave", "honey"],
             fruity: ["orange slice", "cassis", "cherry"]
         };
 
-    $('form').on('submit', function(e) {
+    $('form').on('submit', function(e) { // submit function that call on values when click to generate the drink
         orderValues = [];
-        e.preventDefault();
+        e.preventDefault(); // prevent page from reloading
 
         $('select').each(function() {
             orderValues.push($(this).val() === 'yes' ? true : false);
